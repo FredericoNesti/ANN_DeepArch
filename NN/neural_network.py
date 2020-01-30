e41b6936a6775af87f364e5e93f4dfa1331a9307
theo2021 <theo2021@gmail.com>
2020-01-30 15:06:19 +0100

    fixes

--- NN/neural_network.py
+++ NN/neural_network.py
@@ -9,16 +9,39 @@ class RbfTransformation:
     def __call__(self, inp):
         ans = np.zeros(len(self.means))
         for i, (mean, sigma) in enumerate(zip(self.means, self.sigmas)):
-            ans[i] = np.exp(-(np.linalg.norm(inp - mean)**2)/sigma)
+            ans[i] = np.exp(-(np.inner(inp - mean, inp - mean))/sigma/2)
         return ans
+
+    def train_mu(self, data, step, epochs):
+        for epoch in range(epochs):
+            for sample in np.random.choice(data, len(data)):
+                minn = np.inf
+                minn_i = -1
+                for i in range(len(self.means)):
+                    distance = np.linalg.norm(sample-self.means[i])
+                    if minn > distance:
+                        minn = distance
+                        minn_i = i
+                self.means[minn_i] += step*(sample-self.means[minn_i])
 
 
 class Sigmoid:
+    def __init__(self, slope=1):
+        self.slope = slope
+
     def func(self, x_vec):
-        return 2.0 / (1.0 + np.exp(-x_vec)) - 1
+        return 2.0 / (1.0 + np.exp(-self.slope*x_vec)) - 1
 
     def dev_func(self, x_vec):
-        return (1 + self.func(x_vec)) * (1 - self.func(x_vec)) / 2
+        return ((1 + self.func(x_vec)) * (1 - self.func(x_vec)) / 2)*self.slope
+
+
+class Relu:
+    def func(self, x_vec):
+        return (x_vec > 0)*1*x_vec
+
+    def dev_func(self, x_vec):
+        return (x_vec > 0)*1
 
 
 class Linear:
@@ -53,8 +76,8 @@ class Layer:
     def update_weights(self, dw, momentum):
         if self.fixed_weights:
             return 0
-        self.W += self.dw[:, :-1]*momentum + (1-momentum)*dw[:, :-1]
-        self.bias += self.dw[:, -1]*momentum + (1-momentum)*dw[:, -1]
+        self.W += self.dw[:, :-1]*momentum + dw[:, :-1]  # the lab 1 says (1-momentum)* but i believe is incorrect
+        self.bias += self.dw[:, -1]*momentum + dw[:, -1]
         self.dw = dw
 
     def feed(self, inp):
@@ -75,8 +98,8 @@ class NN:
         o = trans(np.zeros(self.signal_dim[0]))
         self.signal_dim[0] = len(o)
 
-    def add_layer(self, nodes, function=Sigmoid()):
-        self.layers.append(Layer(self.signal_dim[-1], nodes, function))
+    def add_layer(self, nodes, function=Sigmoid(), **args):
+        self.layers.append(Layer(self.signal_dim[-1], nodes, function, **args))
         self.signal_dim.append(nodes)
 
     def feed_forward(self, inp):
@@ -111,16 +134,34 @@ class NN:
         for i, new_dw_layer in enumerate(new_dw):
             self.layers[i].update_weights(step*new_dw_layer, momentum)
 
-    def train_batch(self, inputs, targets, batch_size, epochs, step, momentum):
+    def train_batch(self, inputs, targets, batch_size, epochs, step, momentum, learning_curve=False, test_set=None, test_targets=None):
+        training_error = []
+        test_error = []
         loops = int(len(inputs)/batch_size)
         for epoch in range(0, epochs):
-            for i in range(1, loops):
-                inp_batch = inputs[batch_size*(i-1): batch_size*i]
-                trg_batch = targets[batch_size*(i-1): batch_size*i]
+            prev = 0
+            for i in range(0, loops):
+                inp_batch = inputs[prev: batch_size*(i+1)]
+                trg_batch = targets[prev: batch_size*(i+1)]
                 new_dw = self.dw_batch(inp_batch, trg_batch)
                 self.update_dw(new_dw, step, momentum)
+                prev = batch_size*(i+1)
             inp_batch = inputs[batch_size * loops:]
             trg_batch = targets[batch_size * loops:]
             new_dw = self.dw_batch(inp_batch, trg_batch)
             self.update_dw(new_dw, step, momentum)
+            if learning_curve:
+                error = 0
+                for s, t in zip(inputs, targets):
+                    error += np.abs(self.feed_forward(s)[1][0]-t)
+                error = error/len(inputs)
+                training_error.append(error)
+            error = 0
+            if test_set is not None:
+                for s, t in zip(test_set, test_targets):
+                    error += np.abs(self.feed_forward(s)[1][0]-t)
+                error = error / len(test_set)
+                test_error.append(error)
+        return training_error, test_error
+
 
