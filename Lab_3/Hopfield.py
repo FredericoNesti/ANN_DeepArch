fa015f96d8f75f19ea84a1fa062c3ed6185ef9e0
matheusoliveirafranca <matheusfranca.t19@gmail.com>
2020-02-08 17:00:11 +0100

    task 3.1 and 3.3

--- Lab_3/Hopfield.py
+++ Lab_3/Hopfield.py
@@ -1,6 +1,6 @@
 import numpy as np
 import itertools as it
-
+import matplotlib.pyplot as plt
 
 def sign(x):
     if x >= 0:
@@ -19,22 +19,46 @@ class Hopfield():
             wij = 1/N sum from mu=1 to P of x_i^mu x_j^mu
         """
         weights = np.matmul(input_mem_patterns.T, input_mem_patterns)
-        return weights / self.n_nodes
+        # return weights / self.n_nodes  # good initialization
+        # return np.random.normal(0, 1, (self.n_nodes,self.n_nodes))  # random init
+        return 0.5 * (np.random.normal(0, 1, (self.n_nodes, self.n_nodes)) +
+                      np.random.normal(0, 1, (self.n_nodes, self.n_nodes)))  # random, but symetric
 
     def update_rule(self, pattern):
         """
-        Pertorms the update on weights as following:
-            xi sign ( sum over j of wij xj )
+            Pertorms the update on weights as following:
+                xi sign ( sum over j of wij xj )
         """
         return np.apply_along_axis(lambda t: sign(np.dot(t, pattern)), 1, self.weights)
 
-    def update_till_convergence(self, pattern):
+    def update_till_convergence(self, pattern, follow_energy=False):
+
+        if follow_energy:
+            energy = [self.energy(pattern)]
+
         next_pattern = self.update_rule(pattern)
-        while (np.all(next_pattern != pattern)):
+        max_int = 20
+        while (not np.all(next_pattern == pattern) and max_int > 0):
+            if follow_energy:
+                energy.append(self.energy(next_pattern))
+
             pattern = next_pattern
             next_pattern = self.update_rule(pattern)
+            max_int -= 1
+
+        if follow_energy:
+            interaction = [i for i in range(len(energy))]
+            plt.plot(interaction, energy, label="Energy")
+            plt.legend(bbox_to_anchor=(0.05, .95), loc='upper left', borderaxespad=0.)
+            plt.show()
 
         return next_pattern
+
+    def energy(self, pattern):
+        """
+            return the following energy function E = −sum_i sum_j wi*jx_i*x_j
+        """
+        return -np.dot(np.matmul(self.weights, pattern), pattern)
 
 
 input_mem_patterns = np.array([[-1, -1, 1, -1, 1, -1, -1, 1],
@@ -53,15 +77,40 @@ xd = np.array([[1, -1, 1, -1, 1, -1, -1, 1],
                [1, 1, -1, -1, -1, 1, -1, -1],
                [1, 1, 1, -1, 1, 1, -1, 1]])
 
-for x in xd:
-    print("Input pattern:", x)
-    print("Output pattern:", hp.update_till_convergence(x))
-    print("")
-attractors = set()
-for sample in list(it.product([-1, 1], repeat=8)):
-    ts = hp.update_till_convergence(sample)
-    attractors.add(np.array2string(ts))
-print("ATTRACTORS: n=" + str(len(attractors)))
-for at in attractors:
-    print(at)
-print("------")
+# for x in xd:
+#     print("Input pattern:", x)
+#     print("Output pattern:", hp.update_till_convergence(x))
+#     print("")
+# attractors = set()
+# for sample in list(it.product([-1, 1], repeat=8)):
+#     ts = hp.update_till_convergence(sample)
+#     attractors.add(np.array2string(ts))
+# print("ATTRACTORS: n=" + str(len(attractors)))
+# for at in attractors:
+#     print(at)
+# print("------")
+
+## Task 3.2 Sequential Update
+patterns = np.loadtxt('pict.dat', dtype=int, delimiter=',').reshape(11,1024)
+print("")
+
+## Task 3.3 Energy
+# print("Task 3.3")
+
+# print("Energy of distorted patterns")
+# for pat in xd:
+#     print("pattern:", pat, "Energy:", hp.energy(pat))
+
+#
+# print("Following energy with random weights")  # TODO: We must update the input sequentially as in Task 3.2
+# for pat in xd:
+#     hp.update_till_convergence(pat, follow_energy=True)
+#
+# print("Conclusion: the energy doesnt always decrease and doesnt converge")
+#
+#
+# print("Following energy with random, but symetric, weights")
+# for pat in xd:
+#     hp.update_till_convergence(pat, follow_energy=True)
+#
+# print("Conclusion: it is unstable and can increase, but it converges")
